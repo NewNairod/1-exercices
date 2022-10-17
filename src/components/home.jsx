a8880a947392d2f51a290196e4cb2dcd5ef7c7f1
Libotte Dorian <d.libotte78@gmail.com>
2022-10-17 10:59:05 +0200

    test 1

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,1 +1,31 @@
 import React from 'react';
+export default class Home extends React.Component{
+    constructor(props)
+{
+    super(props);
+    this.state = {
+        nom:'',
+        prenom:''
+    }
+    this.handleSubmit=this.handleSubmit.bind(this);
+}
+onChange= e=> {
+    this.setState({ [e.target.name] :e.target.value});
+};
+handleSubmit(event)
+{
+    alert("Bonjour "+this.state.nom+" "+ this.state.prenom);
+    event.preventDefault();
+}
+render(){
+    return(
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <h1>Profil de {this.state.nom+" "+this.state.prenom}</h1>
+                Nom:<input type="text" value={this.state.nom} onChange={this.onChange} name="nom"></input>
+                Prenom:<input type="text" value={this.state.prenom} name="prenom" onChange={this.onChange}></input>
+                <button type="submit" >Afficher message</button>
+            </form>
+        </div>);
+}
+}
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -12,7 +12,6 @@ return (
 </li>
 </ul>
 </nav>
-<Outlet />
 </>
 )
 };
